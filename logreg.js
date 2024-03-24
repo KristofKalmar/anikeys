3f4a69c7d5de8e8670c5711395d2690be681e92c
Kalmár Kristóf <kristof.kalmar2004@gmail.com>
2024-03-24 19:40:35 +0100

    asd

--- logreg.js
+++ logreg.js
@@ -19,19 +19,19 @@ const showHiddenPass = (loginPass, loginEye) =>{
           iconEye = document.getElementById(loginEye);
 
     iconEye.addEventListener('click', () =>{
-       
+
        if(input.type === 'password'){
-          
+
           input.type = 'text';
 
-          
+
           iconEye.classList.add('bxs-lock-open-alt');
           iconEye.classList.remove('bxs-lock-alt');
        } else{
-          
+
           input.type = 'password';
 
-          
+
           iconEye.classList.remove('bxs-lock-open-alt');
           iconEye.classList.add('bxs-lock-alt');
        }
@@ -48,20 +48,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const loginButton = loginForm.querySelector('.btn');
     const usernameInput = loginForm.querySelector('input[type="text"]');
     const passwordInput = loginForm.querySelector('input[type="password"]');
-    
+
     loginButton.addEventListener('click', function (event) {
-        event.preventDefault(); 
-        
+        /*event.preventDefault();
+
         const uname = usernameInput.value.trim();
         const password = passwordInput.value.trim();
-        
+
         if (!uname || !password) {
             alert('Kérjük, töltse ki mind a felhasználónevet, mind a jelszót!');
             return;
         }
-     
-        alert('Sikeres bejelentkezés!');
-        
-        window.location.href = 'index.html';
+
+        alert('Sikeres bejelentkezés!');*/
+
+        window.location.href = 'profil.html';
     });
 });
